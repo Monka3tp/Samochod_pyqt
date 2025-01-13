72c87cb855e09ce187a74f1388a0953bca58f35b
Wilkolak3 <patrykjanusz@onet.pl>
2025-01-13 15:06:33 +0100

    kod 13.01

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 
 from PyQt6.QtCore import QDate
-from PyQt6.QtWidgets import QDialog, QApplication
+from PyQt6.QtWidgets import QDialog, QApplication, QMessageBox
 
 from layout import Ui_Dialog
 
-
 class MyForm(QDialog):
+
+    prices = {
+        'miejski': 80,
+        'kompakt:': 120,
+        'van': 140,
+        'suv': 160
+    }
+
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
@@ -21,8 +28,13 @@ class MyForm(QDialog):
         end_date = self.ui.endDate.date()
 
         #legalność dat
-        if start_date.daysTo(end_date) < 0:
-            print('rerr')
+        if start_date.daysTo(end_date) < 0 and start_date.daysTo(QDate.currentDate()) >= 0:
+            message_box = QMessageBox()
+            message_box.setWindowTitle('Błąd')
+            message_box.setInformativeText("Data rozpoczęcia najmu jest późniejsza niż data jego zakończenia")
+            message_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+            message_box.exec()
+            return
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
